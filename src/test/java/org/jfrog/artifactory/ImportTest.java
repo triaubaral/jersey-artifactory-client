dcb21794a5bf2f274b006b0dc6320c4fa9bb3909
Aurelien Tricoire <aurelien.tricoire@gmail.com>
2013-08-29 11:49:59 +0200

    Modify import methodology

--- src/test/java/org/jfrog/artifactory/ImportTest.java
+++ src/test/java/org/jfrog/artifactory/ImportTest.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.jfrog.artifactory.api.impl.ArtifactoryAPI;
+import org.jfrog.artifactory.api.impl.ArtifactoryAPIImpl;
 import org.jfrog.artifactory.param.WrongBasicAuthParamBuilder;
 import org.jfrog.artifactory.param.WrongHomeUrlParamBuilder;
 import org.jfrog.artifactory.param.WrongPathParamBuilder;
@@ -15,7 +15,7 @@ import org.jfrog.artifactory.param.WrongRepositoryParamBuilder;
 import org.junit.AfterClass;
 import org.junit.Test;
 
-public class ImportTest extends ArtifactoryUtilsTest{
+public class ImportTest extends ArtifactoryAPITest{
 	
 	private static Logger logger = Logger.getLogger(ImportTest.class);	
 	
@@ -30,7 +30,7 @@ public class ImportTest extends ArtifactoryUtilsTest{
 		logger.info("testWrongHomeUrlFileUpload");
 		
 		try{					
-			testUpload(testArtifact, new ArtifactoryAPI(new WrongHomeUrlParamBuilder()));
+			testUpload(testArtifact, new ArtifactoryAPIImpl(new WrongHomeUrlParamBuilder()));
 			fail("Should have thrown ArtifactoryUtilsException for wrong ARTIFACTORY_URL");
 		}
 		catch(ArtifactoryUtilsException e){
@@ -46,7 +46,7 @@ public class ImportTest extends ArtifactoryUtilsTest{
 		logger.info("testWrongRepositoryFileUpload");
 		
 		try{			
-			testUpload(testArtifact, new ArtifactoryAPI(new WrongRepositoryParamBuilder()));
+			testUpload(testArtifact, new ArtifactoryAPIImpl(new WrongRepositoryParamBuilder()));
 			fail("Should have thrown ArtifactoryUtilsException for wrong REPOSITORY");
 		}
 		catch(ArtifactoryUtilsException e){
@@ -58,7 +58,7 @@ public class ImportTest extends ArtifactoryUtilsTest{
 	@Test
 	public void testWrongPathFileUpload(){
 		logger.info("testWrongPathFileUpload");
-		testUpload(testArtifact, new ArtifactoryAPI(new WrongPathParamBuilder()));		
+		testUpload(testArtifact, new ArtifactoryAPIImpl(new WrongPathParamBuilder()));		
 		//A path is created at the wrong path and it does not throw any error.		
 	}
 	
@@ -66,7 +66,7 @@ public class ImportTest extends ArtifactoryUtilsTest{
 	public void testWrongBasicAuthFileUpload(){
 		logger.info("testWrongBasicAuthFileUpload");
 		try{			
-			testUpload(testArtifact, new ArtifactoryAPI(new WrongBasicAuthParamBuilder()));
+			testUpload(testArtifact, new ArtifactoryAPIImpl(new WrongBasicAuthParamBuilder()));
 			fail("Should have thrown ArtifactoryUtilsException for wrong LOGIN/PASSWORD");
 		}
 		catch(ArtifactoryUtilsException e){
@@ -79,13 +79,8 @@ public class ImportTest extends ArtifactoryUtilsTest{
 	public void testRegularImport() {	
 		logger.info("testRegularImport");
 		testUpload(testArtifact, artifactoryAPI);
-	}
+	}	
 	
-	@Test
-	public void testCheckSumUpload(){
-		logger.info("testCheckSumUpload");		
-		artifactoryAPI.importFromWithChecksum(testArtifact);		
-	}
 	
 	@AfterClass
 	public static void afterClass(){
@@ -94,7 +89,7 @@ public class ImportTest extends ArtifactoryUtilsTest{
 		
 	}
 	
-	private void testUpload(final File pfile, final ArtifactoryAPI pPartifactoryAPI){		
+	private void testUpload(final File pfile, final ArtifactoryAPIImpl pPartifactoryAPI){		
 		pPartifactoryAPI.importFrom(pfile);		
 	}	
 	
